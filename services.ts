88bd6d37f90a11039c4b1e176b6578e8ac5dc5b1
netnovaz <61082674+netnovaz@users.noreply.github.com>
2020-02-25 09:46:40 +0530

    Create services.ts

---
+++ services.ts
@@ -0,0 +1,62 @@
+import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/map';
+
+/*
+  Generated class for the InAppBrowserServiceProvider provider.
+
+  See https://angular.io/guide/dependency-injection for more info on providers
+  and Angular DI.
+*/
+@Injectable()
+export class InAppBrowserServiceProvider {
+
+  private apiUrl = 'http://netnovaz.com/nizamuddin-app/apis/articles.json';
+  private photoGallery = 'http://netnovaz.com/nizamuddin-app/apis/photo-gallery.json';
+  constructor(public http: Http) {
+    console.log('Hello InAppBrowserServiceProvider Provider');
+  }
+
+  public open(url: string): boolean {
+		window.open(url, '_system', 'location=yes');
+		return false;
+  }
+  
+  getLatest() {  
+        let latestList = this.http.get('http://netnovaz.com/nizamuddin-app/apis/data.json');
+        return latestList;
+    }
+	
+  getArticles(): Observable<string[]> {
+    return this.http.get(this.apiUrl)
+                    .map(this.extractData)
+                    .catch(this.handleError);
+  }
+
+  getPhotoGallery(): Observable<string[]> {
+    return this.http.get(this.photoGallery)
+                    .map(this.extractData)
+                    .catch(this.handleError);
+  }
+
+  private extractData(res: Response) {
+    let body = res.json();
+    return body || { };
+  }
+
+  private handleError (error: Response | any) {
+    let errMsg: string;
+    if (error instanceof Response) {
+      const body = error.json() || '';
+      const err = body.error || JSON.stringify(body);
+      errMsg = `${error.status} - ${error.statusText || ''} ${err}`;
+    } else {
+      errMsg = error.message ? error.message : error.toString();
+    }
+    console.error(errMsg);
+    return Observable.throw(errMsg);
+  }
+
+}
